31b77e9e9683d95f1391b6f8a20f8a366f5eb7bf
Gelissean <gelu586@gmail.com>
2021-10-24 18:23:37 +0200

    finalised p3

--- KrypBz/proj3/prudova.py
+++ KrypBz/proj3/prudova.py
@@ -1,7 +1,9 @@
+import os
+
 from KrypBz.proj3.square_generator import square_generator
-from KrypBz.tools.char_ops import coincidence_index_comparison, move_char
+from KrypBz.tools.char_ops import coincidence_index_comparison, move_char, coincidence_index
 from KrypBz.tools.cipher_tools import convert
-from KrypBz.tools.file_tools import get_file_paths, read_file, convert_to_ascii, write_to_file
+from KrypBz.tools.file_tools import get_file_paths, read_file, convert_to_ascii, write_to_file, make_directory
 from KrypBz.tools.coincidence_indices import coincidence_indices
 
 
@@ -9,32 +11,34 @@ def translate(ciphered, a, b, m, x0, buffer_size=100):
     generator = square_generator(a, b, m, x0)
     retval = []
     for c in ciphered:
-        retval.append(move_char(c, -generator.get_actual(), 26))
+        retval.append(move_char(c, -int(26 * generator.get_actual()), 26))
         generator.generate_next()
     return ''.join(retval)
 
 
-def brute_force(subor, a, b, m, epsilon, keys):
+def brute_force(subor, cesta_ciel, a, b, m, epsilon):
     original = read_file(subor)
     ciphered = convert_to_ascii(original)
-    #TODO po poslednu / #folder_path = subor.get_path
-    for i in range(88888,m):
+    original_indices = coincidence_indices.get_all_power()
+    head, tail = os.path.split(subor)
+    cesta = cesta_ciel + '/' + tail
+    make_directory(cesta)
+    for i in range(m):
         attempt = translate(ciphered, a, b, m, i)
-        for index in keys:
-            if coincidence_index_comparison(attempt, 26, coincidence_indices.get(index)) <= epsilon:
-                #write_to_file(convert(original, attempt), folder_path + '/' + index + '_' + str(i))
-                write_to_file(convert(original, attempt), "./cv4/" + index + '_' + str(i))
+        coinc_index = coincidence_index(attempt, 26)
+        for key in original_indices:
+            if abs(coinc_index - original_indices[key]) <= epsilon:
+                write_to_file(convert(original, attempt), cesta + '/' + key + '_' + str(i) + '.txt')
 
 
-def prudova(cesta, a, b, m, epsilon, keys):
-    subory = get_file_paths(cesta)
-    subory = ["./cv4/sprava_enc.txt"]
+def prudova(cesta_zdroj, cesta_ciel, a, b, m, epsilon):
+    subory = get_file_paths(cesta_zdroj)
     for subor in subory:
         print("Analysing file: \t" + str(subor))
-        brute_force(subor, a, b, m, epsilon, keys)
+        brute_force(subor, cesta_ciel, a, b, m, epsilon)
     print("Done")
 
 
 if __name__ == '__main__':
-    keys = ["sk", "en"]
-    prudova("./cv4/", 8121, 28411, 134456, 0.005, keys)
+    # prudova("./cv4", "./cv4", 8121, 28411, 134456, 0.01)  # stream cipher
+    prudova("./sifrovane", "./desifrovane", 84589, 45989, 217728, 0.01)  # stream cipher

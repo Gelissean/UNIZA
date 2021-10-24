ab7e1849a440405e7054634a12c8b9a11c74cd9b
Gelissean <gelu586@gmail.com>
2021-10-24 18:22:20 +0200

    fixed generator

--- KrypBz/proj3/square_generator.py
+++ KrypBz/proj3/square_generator.py
@@ -6,11 +6,11 @@ class square_generator:
         self.x = x0
 
     def get_actual(self):
-        return self.x
+        return self.x / self.m
 
     def get_next(self):
         self.generate_next()
-        return self.get_actual()
+        return self.get_actual() / self.m
 
     def generate_next(self):
         self.x = (self.a * self.x + self.b) % self.m
